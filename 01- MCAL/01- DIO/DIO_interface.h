58a16bb21499265148af83493e2c50a62dc2b9c6
AgamalARM <agamalarm@gmail.com>
2025-04-20 15:29:43 +0200

    DIO_Driver V2

--- 01- MCAL/01- DIO/DIO_interface.h
+++ 01- MCAL/01- DIO/DIO_interface.h
@@ -1,14 +1,38 @@
 #ifndef DIO_INTERFAC_H
 #define DIO_INTERFAC_H
 
+typedef enum{
+	DIO_NOK,
+	DIO_OK
+	
+}DIO_ErrorStatus ;
+
 #define DIO_PORTA   0
 #define DIO_PORTB   1
 #define DIO_PORTC   2
 #define DIO_PORTD   3
 
+#define DIO_PIN0   0
+#define DIO_PIN1   1
+#define DIO_PIN2   2
+#define DIO_PIN3   3
+#define DIO_PIN4   4
+#define DIO_PIN5   5
+#define DIO_PIN6   6
+#define DIO_PIN7   7
+
+#define DIO_OUTPUT  1
+#define DIO_INPUT   0
+
+#define DIO_HIGH  1
+#define DIO_LOW   0
+
 /* Function prototype*/
-void DIO_voidSetPortDirection( u8 Copy_u8Port , u8 Copy_u8Direction );
-void DIO_voidSetPortValue( u8 Copy_u8Port , u8 Copy_u8Value );
+DIO_ErrorStatus DIO_enumSetPortDirection ( u8 Copy_u8Port , u8 Copy_u8Direction );
+DIO_ErrorStatus DIO_enumSetPortValue     ( u8 Copy_u8Port , u8 Copy_u8Value )    ;
+
+DIO_ErrorStatus DIO_enumSetPinDirection  ( u8 Copy_u8Port , u8 Copy_u8Pin , u8 Copy_u8Direction );
+DIO_ErrorStatus DIO_enumSetPintValue     ( u8 Copy_u8Port , u8 Copy_u8Pin , u8 Copy_u8Value )    ;
 
 
 #endif
--- 01- MCAL/01- DIO/DIO_program.c
+++ 01- MCAL/01- DIO/DIO_program.c
@@ -7,28 +7,62 @@
 
  
  
-void DIO_voidSetPortDirection( u8 Copy_u8Port , u8 Copy_u8Direction ){
-	if ( Copy_u8Port >= DIO_PORTA && Copy_u8Port <= DIO_PORTD){
-		switch ( Copy_u8Port ){
-			case DIO_PORTA: DDRA = Copy_u8Direction ; break; 
-			case DIO_PORTB: DDRB = Copy_u8Direction ; break; 
-			case DIO_PORTC: DDRC = Copy_u8Direction ; break; 
-			case DIO_PORTD: DDRD = Copy_u8Direction ; break; 
-		}
-		
-	}
+DIO_ErrorStatus DIO_enumSetPortDirection( u8 Copy_u8Port , u8 Copy_u8Direction ){
 	
-}
-void DIO_voidSetPortValue( u8 Copy_u8Port , u8 Copy_u8Value ){
-	if ( Copy_u8Port >= DIO_PORTA && Copy_u8Port <= DIO_PORTD){
+	DIO_ErrorStatus LOC_enumState = DIO_OK ;
+	
 		switch ( Copy_u8Port ){
-			case DIO_PORTA: PORTA = Copy_u8Value ; break; 
-			case DIO_PORTB: PORTB = Copy_u8Value ; break; 
-			case DIO_PORTC: PORTC = Copy_u8Value ; break; 
-			case DIO_PORTD: PORTD = Copy_u8Value ; break; 
+			case DIO_PORTA: DDRA_REG = Copy_u8Direction ; break; 
+			case DIO_PORTB: DDRB_REG = Copy_u8Direction ; break; 
+			case DIO_PORTC: DDRC_REG = Copy_u8Direction ; break; 
+			case DIO_PORTD: DDRD_REG = Copy_u8Direction ; break; 
+			default       : LOC_enumState = DIO_NOK ; break;   
 		}
 		
-	}
+	return LOC_enumState ;
+		
 	
 	
 }
+DIO_ErrorStatus DIO_enumSetPortValue( u8 Copy_u8Port , u8 Copy_u8Value ){
+	
+	DIO_ErrorStatus LOC_enumState = DIO_OK ;
+	
+		switch ( Copy_u8Port ){
+			case DIO_PORTA: PORTA_REG = Copy_u8Value     ; break; 
+			case DIO_PORTB: PORTB_REG = Copy_u8Value     ; break; 
+			case DIO_PORTC: PORTC_REG = Copy_u8Value     ; break; 
+			case DIO_PORTD: PORTD_REG = Copy_u8Value     ; break; 
+			default       : LOC_enumState = DIO_NOK  ; break;
+		}
+		
+	
+	return LOC_enumState ;
+	
+}
+
+DIO_ErrorStatus DIO_enumSetPinDirection(u8 Copy_u8Port , u8 Copy_u8Pin , u8 Copy_u8Direction ){
+	DIO_ErrorStatus LOC_enumState = DIO_OK ;
+	if ( Copy_u8Pin >= DIO_PIN0 && Copy_u8Pin <= DIO_PIN7 ) {
+		
+	}else{
+		LOC_enumState = DIO_NOK ;
+	}
+	
+	return LOC_enumState ;	
+}
+DIO_ErrorStatus DIO_enumSetPintValue(u8 Copy_u8Port , u8 Copy_u8Pin , u8 Copy_u8Value){
+	DIO_ErrorStatus LOC_enumState = DIO_OK ;
+	
+	return LOC_enumState ;	
+	
+}
+
+
+
+
+
+
+
+
+
